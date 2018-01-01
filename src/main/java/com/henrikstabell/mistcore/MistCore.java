a7197284fdda5ec97a9ac7f1241955030acee766
Ole Henrik Stabell <ole@henrikstabell.com>
2018-01-01 11:20:59 +0100

    Make sure misthandler only runs clientside fixes #6
    mod info changes, version bump
    NOTE: this fix will be backported to older versions!

--- src/main/java/com/henrikstabell/mistcore/MistCore.java
+++ src/main/java/com/henrikstabell/mistcore/MistCore.java
@@ -5,17 +5,21 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(
-   modid = "mistcore",
-   version = "1.0"
-)
+@Mod(modid = this.MODID, version = this.VERSION)
 public class MistCore {
    public static final String MODID = "mistcore";
-   public static final String VERSION = "1.0";
+   public static final String VERSION = "1.2.0";
 
    @EventHandler
    public void preInit(FMLPreInitializationEvent event) {
+      RegisterClientHandlers();
+   }
+
+   @SideOnly(Side.CLIENT)
+   public static void RegisterClientHandlers() {
       MinecraftForge.EVENT_BUS.register(new MistHandler());
    }
 }
