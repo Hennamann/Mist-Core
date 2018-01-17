96829f5db10164f141f7222fb3e88a815cb490f2
Ole Henrik Stabell <ole@henrikstabell.com>
2018-01-17 17:35:25 +0100

    Sync with master

--- src/main/java/com/henrikstabell/mistcore/MistCore.java
+++ src/main/java/com/henrikstabell/mistcore/MistCore.java
@@ -1,18 +1,15 @@
 package com.henrikstabell.mistcore;
 
-import com.henrikstabell.mistcore.api.MistHandler;
+import com.henrikstabell.mistcore.handler.MistHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(
-   modid = "mistcore",
-   version = "1.0"
-)
+@Mod(modid = MistCore.MODID, version = MistCore.VERSION)
 public class MistCore {
    public static final String MODID = "mistcore";
-   public static final String VERSION = "1.0";
+   public static final String VERSION = "1.2.1";
 
    @EventHandler
    public void preInit(FMLPreInitializationEvent event) {
