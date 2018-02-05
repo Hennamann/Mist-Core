5fb9ba6c4bf6dc3f130b2d843f46998a48f6bd0d
Ole Henrik Stabell <ole@henrikstabell.com>
2018-02-05 18:51:09 +0100

    sync with master

--- src/main/java/com/henrikstabell/mistcore/api/package-info.java
+++ src/main/java/com/henrikstabell/mistcore/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "mistcore", apiVersion = "1.1.2", provides = "mistcore|API")
+@API(owner = "mistcore", apiVersion = "1.2.1", provides = "mistcore|API")
 package com.henrikstabell.mistcore.api;
 
 import cpw.mods.fml.common.API;
